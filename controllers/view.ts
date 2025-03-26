d8d610bd345458a4974684389e8828a55b447d81
nolazheng <mania001988@gmail.com>
2025-03-26 15:25:42 +0800

    feat: support tour map

--- controllers/view.ts
+++ controllers/view.ts
@@ -21,8 +21,14 @@ export const getTourView = catchAsyncError(async (req, res, next) => {
     return next(createAppError(`No document found with that ID`, 404));
   }
 
-  res.status(200).render('tour', {
-    title: req.params.slug,
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: req.params.slug,
+      tour,
+    });
 });
---
+++ public/js/mapbox.js
@@ -0,0 +1,47 @@
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+/* eslint-disable */
+// export const displayMap = (locations) => {
+mapboxgl.accessToken =
+  'pk.eyJ1IjoianVuZXlpbmciLCJhIjoiY204cGpjbHh2MGM0MzJxcTRnaTFjYTZpcyJ9.0OQS6bU5N-eJazXCbYLe-w';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/juneying/cm8pk50mj00bf01rdacg1gatk',
+  scrollZoom: false,
+  // center: [-118, 34], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  // zoom: 9, // starting zoom
+  // interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
+// };
